e372cb43245dd3da8974219f31aae7a734cb5640
arodrigue003 <adrien.rod@aliceadsl.fr>
2023-12-21 22:25:23 +0100

    Add: Add day 21 part 02 solution

--- src/day21/logic.rs
+++ src/day21/logic.rs
@@ -3,7 +3,11 @@ use bucket_queue::{BucketQueue, FirstInFirstOutQueue};
 use ndarray::Array2;
 use std::collections::VecDeque;
 
-fn count_accessible_tiles(map: &Map, start: (usize, usize), steps: usize) -> usize {
+fn count_accessible_tiles(map: &Map, start: (usize, usize), steps: usize, parity: usize) -> usize {
+    if steps == 0 {
+        return 0;
+    }
+
     // Use the Dijkstra in order to determinate the distance we can travel in steps steps.
 
     // Create structures to support the algorithm
@@ -42,20 +46,210 @@ fn count_accessible_tiles(map: &Map, start: (usize, usize), steps: usize) -> usi
             scores[[line, column + 1]] = score + 1;
             visit_next.enqueue((score + 1, line, column + 1, remaining_steps - 1), score + 1);
         }
+
+        visited[[line, column]] = true;
     }
 
     // The result is equal to the number of distance that has the same remainder as steps by 2 as steps.
-    let remainder = steps % 2;
     scores
         .iter()
-        .filter(|score| **score != usize::MAX && **score % 2 == remainder)
+        .filter(|score| **score != usize::MAX && **score % 2 == parity)
         .count()
 }
 
 pub fn solve_part_one(map: &Map) -> usize {
-    count_accessible_tiles(map, map.start, 64)
+    count_accessible_tiles(map, map.start, 64, 0)
 }
 
-pub fn solve_part_two(map: &Map) -> u32 {
-    0
+#[allow(dead_code)]
+fn solve_part_two_bruteforce(map: &Map, start: (usize, usize), steps: usize) -> usize {
+    // Compute how many maps we will need for the bruteforce
+    let map_count = 2 * (steps / map.width) + 1;
+
+    // Build a map that is a concatenation of every map
+    let mut grid = vec![vec![false; map.width * map_count]; map.width * map_count];
+
+    for map_line in 0..map_count {
+        for map_column in 0..map_count {
+            for line in 0..map.height {
+                for column in 0..map.width {
+                    grid[map.height * map_line + line][map.width * map_column + column] = map.grid[line][column];
+                }
+            }
+        }
+    }
+
+    let start = (
+        map_count / 2 * map.height + start.0,
+        map_count / 2 * map.width + start.1,
+    );
+    let new_map = Map::new(grid, start.clone());
+    // println!("{new_map}");
+
+    count_accessible_tiles(&new_map, start, steps, steps % 2)
+}
+
+pub fn solve_part_two(map: &Map) -> usize {
+    let steps = 26501365;
+    let parity = steps % 2;
+
+    // Verify assertions
+    if map.width != map.height {
+        panic!("Map must be a square!")
+    }
+    if map.width % 2 == 0 {
+        panic!("Map width must be odd!")
+    }
+    if map.start.0 != map.height / 2 || map.start.0 != map.width / 2 {
+        panic!("Start must be at the center!")
+    }
+    if (0..map.width).any(|column| map.grid[0][column]) {
+        panic!("First line must be empty!")
+    }
+    if (0..map.width).any(|column| map.grid[map.height / 2][column]) {
+        panic!("Middle line must be empty!")
+    }
+    if (0..map.width).any(|column| map.grid[map.height - 1][column]) {
+        panic!("Last line must be empty!")
+    }
+    if (0..map.height).any(|line| map.grid[line][0]) {
+        panic!("First column must be empty!")
+    }
+    if (0..map.height).any(|line| map.grid[line][map.width / 2]) {
+        panic!("Middle column must be empty!")
+    }
+    if (0..map.height).any(|line| map.grid[line][map.width - 1]) {
+        panic!("Last column must be empty!")
+    }
+
+    // Compute the number of full square that we will get on the main line / column
+    let main_line_length = steps / map.width;
+
+    // Compute the number of positions for a full map
+    let full_map_positions_count_base_par = count_accessible_tiles(map, map.start, map.width, parity);
+    let full_map_positions_count_other_par = count_accessible_tiles(map, map.start, map.width, (parity + 1) % 2);
+
+    // Compute how many steps will be left on the left map
+    let offset_at_line_end = steps - main_line_length * map.width + map.width / 2;
+
+    // Compute the number of position for the left, top, right and bottom map
+    let left_map_position_count = count_accessible_tiles(
+        map,
+        (map.start.0, map.width - 1),
+        offset_at_line_end,
+        (parity + main_line_length + 1) % 2,
+    ) + count_accessible_tiles(
+        map,
+        (map.start.0, map.width - 1),
+        offset_at_line_end.saturating_sub(map.width),
+        (parity + main_line_length) % 2,
+    );
+    let top_map_position_count = count_accessible_tiles(
+        map,
+        (map.height - 1, map.start.1),
+        offset_at_line_end,
+        (parity + main_line_length + 1) % 2,
+    ) + count_accessible_tiles(
+        map,
+        (map.height - 1, map.start.1),
+        offset_at_line_end.saturating_sub(map.width),
+        (parity + main_line_length) % 2,
+    );
+    let right_map_position_count = count_accessible_tiles(
+        map,
+        (map.start.0, 0),
+        offset_at_line_end,
+        (parity + main_line_length + 1) % 2,
+    ) + count_accessible_tiles(
+        map,
+        (map.start.0, 0),
+        offset_at_line_end.saturating_sub(map.width),
+        (parity + main_line_length) % 2,
+    );
+    let bottom_map_position_count = count_accessible_tiles(
+        map,
+        (0, map.start.1),
+        offset_at_line_end,
+        (parity + main_line_length + 1) % 2,
+    ) + count_accessible_tiles(
+        map,
+        (0, map.start.1),
+        offset_at_line_end.saturating_sub(map.width),
+        (parity + main_line_length) % 2,
+    );
+
+    // Compute how many will be left on the diagonal corners
+    let offset_at_diagonal_end = steps - (main_line_length - 1) * map.width - 1;
+
+    // Compute diagonal res
+    let upper_left_map_position_count_1 = count_accessible_tiles(
+        map,
+        (map.height - 1, map.height - 1),
+        offset_at_diagonal_end,
+        (parity + main_line_length) % 2,
+    );
+    let upper_left_map_position_count_2 = count_accessible_tiles(
+        map,
+        (map.height - 1, map.height - 1),
+        offset_at_diagonal_end.saturating_sub(map.width),
+        (parity + main_line_length + 1) % 2,
+    );
+    let upper_right_map_position_count_1 = count_accessible_tiles(
+        map,
+        (map.height - 1, 0),
+        offset_at_diagonal_end,
+        (parity + main_line_length) % 2,
+    );
+    let upper_right_map_position_count_2 = count_accessible_tiles(
+        map,
+        (map.height - 1, 0),
+        offset_at_diagonal_end.saturating_sub(map.width),
+        (parity + main_line_length + 1) % 2,
+    );
+    let lower_right_map_position_count_1 =
+        count_accessible_tiles(map, (0, 0), offset_at_diagonal_end, (parity + main_line_length) % 2);
+    let lower_right_map_position_count_2 = count_accessible_tiles(
+        map,
+        (0, 0),
+        offset_at_diagonal_end.saturating_sub(map.width),
+        (parity + main_line_length + 1) % 2,
+    );
+    let lower_left_map_position_count_1 = count_accessible_tiles(
+        map,
+        (0, map.height - 1),
+        offset_at_diagonal_end,
+        (parity + main_line_length) % 2,
+    );
+    let lower_left_map_position_count_2 = count_accessible_tiles(
+        map,
+        (0, map.height - 1),
+        offset_at_diagonal_end.saturating_sub(map.width),
+        (parity + main_line_length + 1) % 2,
+    );
+
+    // Compute how many full grid we have
+    let full_grid_count_base_par = (main_line_length - (main_line_length - 1) % 2).pow(2);
+    let full_grid_count_other_par = (main_line_length - (main_line_length) % 2).pow(2);
+    let inner_diagonal_count = main_line_length - 1;
+    let out_diagonal_count = main_line_length;
+
+    // Sum it up
+    let result = full_map_positions_count_base_par * full_grid_count_base_par
+        + full_map_positions_count_other_par * full_grid_count_other_par
+        + left_map_position_count
+        + right_map_position_count
+        + top_map_position_count
+        + bottom_map_position_count
+        + inner_diagonal_count
+            * (upper_left_map_position_count_1
+                + upper_right_map_position_count_1
+                + lower_left_map_position_count_1
+                + lower_right_map_position_count_1)
+        + out_diagonal_count
+            * (upper_left_map_position_count_2
+                + upper_right_map_position_count_2
+                + lower_left_map_position_count_2
+                + lower_right_map_position_count_2);
+
+    result
 }
--- src/day21/tests.rs
+++ src/day21/tests.rs
@@ -2,23 +2,23 @@ use crate::common::{test_part_one_common, test_part_two_common};
 use crate::day21::Day21;
 
 static INPUT_EXAMPLE: &str = "...........
-.....###.#.
-.###.##..#.
+......##.#.
+.###..#..#.
 ..#.#...#..
 ....#.#....
-.##..S####.
-.##..#...#.
+.....S.....
+.##......#.
 .......##..
 .##.#.####.
-.##..##.##.
+.##...#.##.
 ...........";
 
 #[test]
 fn test_part_one() {
-    test_part_one_common(Day21::default(), INPUT_EXAMPLE, 42);
+    test_part_one_common(Day21::default(), INPUT_EXAMPLE, 47);
 }
 
 #[test]
 fn test_part_two() {
-    test_part_two_common(Day21::default(), INPUT_EXAMPLE, 1);
+    test_part_two_common(Day21::default(), INPUT_EXAMPLE, 528192865877841);
 }
