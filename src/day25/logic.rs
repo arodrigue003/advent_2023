8a06efc6c717ed2d0b8085bc579f9d38ef4eb7c9
arodrigue003 <adrien.rod@aliceadsl.fr>
2023-12-25 11:46:25 +0100

    Perf: Improve day 25 performance

--- src/day25/logic.rs
+++ src/day25/logic.rs
@@ -1,7 +1,8 @@
 use crate::day25::models::Connection;
 use petgraph::graph::EdgeIndex;
-use petgraph::Graph;
+use petgraph::{Graph, Undirected};
 use rand::Rng;
+use rayon::prelude::*;
 use std::collections::HashMap;
 
 pub fn solve_part_one(connections: &[Connection]) -> u32 {
@@ -25,38 +26,45 @@ pub fn solve_part_one(connections: &[Connection]) -> u32 {
         }
     }
 
-    let mut rng = rand::thread_rng();
-
     // Perform the algorithm while the cut is not of size 3
-    loop {
-        let mut new_graph = graph.clone();
+    (0..usize::MAX)
+        .into_par_iter()
+        .filter_map(|_| get_result(&graph))
+        .find_any(|_| true)
+        .unwrap()
+}
 
-        // Use the Karger's algorithm to find the minimal cut
-        while new_graph.node_count() > 2 {
-            // Choose two nodes arbitrary
-            let edge_index = EdgeIndex::new(rng.gen_range(0..new_graph.edge_count()));
+fn get_result(graph: &Graph<u32, i32, Undirected>) -> Option<u32> {
+    let mut rng = rand::thread_rng();
+    let mut new_graph = graph.clone();
 
-            // Get the edge from the graph
-            let (start, end) = new_graph.edge_endpoints(edge_index).unwrap();
+    // Use the Karger's algorithm to find the minimal cut
+    while new_graph.node_count() > 2 {
+        // Choose two nodes arbitrary
+        let edge_index = EdgeIndex::new(rng.gen_range(0..new_graph.edge_count()));
 
-            // Connect every edge to end to start instead
-            let neighbors: Vec<_> = new_graph.neighbors_undirected(end).into_iter().collect();
-            for neighbor in neighbors {
-                if start != neighbor {
-                    new_graph.add_edge(start, neighbor, 1);
-                }
+        // Get the edge from the graph
+        let (start, end) = new_graph.edge_endpoints(edge_index).unwrap();
+
+        // Connect every edge to end to start instead
+        let neighbors: Vec<_> = new_graph.neighbors_undirected(end).into_iter().collect();
+        for neighbor in neighbors {
+            if start != neighbor {
+                new_graph.add_edge(start, neighbor, 1);
             }
-
-            // Update the weight of start to take into account the removal of end
-            new_graph[start] = new_graph[start] + new_graph[end];
-
-            // Pop end
-            new_graph.remove_node(end);
         }
 
-        if new_graph.edge_count() == 3 {
-            return new_graph.node_weights().fold(1, |acc, weight| acc * *weight);
-        }
+        // Update the weight of start to take into account the removal of end
+        new_graph[start] = new_graph[start] + new_graph[end];
+
+        // Pop end
+        new_graph.remove_node(end);
+    }
+
+    if new_graph.edge_count() == 3 {
+        Some(new_graph.node_weights().fold(1, |acc, weight| acc * *weight))
+    } else {
+        None
     }
 }
 
